470038f33f460129cd7704654febba1dda08b0ed
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-09-30 18:03:38 +0000

    Nuevo cambio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const suits = ["♥", "♦", "♣", "♠"];
+const ranks = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function generateRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomRank = ranks[Math.floor(Math.random() * ranks.length)];
+
+  return { suit: randomSuit, rank: randomRank };
+}
+
+function updateCardElement(card, cardElement) {
+  cardElement.querySelector(".top-suit").textContent = card.suit;
+  cardElement.querySelector(".bottom-suit").textContent = card.suit;
+  cardElement.querySelector(".rank").textContent = card.rank;
+}
+
+const cardContainer = document.getElementById("cardContainer");
+const generatedCard = document.getElementById("generatedCard");
+const generateButton = document.getElementById("generateButton");
+
+generateButton.addEventListener("click", function() {
+  const newCard = generateRandomCard();
+  updateCardElement(newCard, generatedCard);
+});
